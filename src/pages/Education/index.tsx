dca54fda6f3c7aa97e87800fc39afcc19841865d
lucasptcastro@gmail.com <lucasptcastro@gmail.com>
2023-03-29 22:11:05 -0300

    ✨ feat: added education page

---
+++ src/pages/Education/index.tsx
@@ -0,0 +1,22 @@
+import Navbar from "../../components/Navbar/index";
+import Footer from "../../components/Footer/index";
+import "./style.css";
+
+const Education: React.FC = () => {
+  return (
+    <>
+      <div className="navbar">
+        <Navbar />
+        <hr />
+      </div>
+      <div className="content">
+        <div></div>
+      </div>
+      {/* <div className="footer">
+        <Footer />
+      </div> */}
+    </>
+  );
+};
+
+export default Education;
