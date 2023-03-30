2ef60a7d8845b950fea918ba725f6ec6c50b4504
lucasptcastro@gmail.com <lucasptcastro@gmail.com>
2023-03-29 22:12:25 -0300

    ✨ feat: added home and education routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import Education from "./pages/Education";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/education",
+    element: <Education />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
