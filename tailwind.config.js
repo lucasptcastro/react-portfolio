3abafe8c951c21f68e2162cf13c6dabe94957e3d
lucasptcastro@gmail.com <lucasptcastro@gmail.com>
2023-04-05 21:51:53 -0300

    ✨ feat: added tailwindcss configs

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{html,js,ts,tsx,jsx}",
+    "./src/components/**/*.{js,jsx,ts,tsx}",
+    "./src/pages/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
